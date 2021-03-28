9a834228504296ef20e835f6ad4e1a79a7e8bc23
Clueskee <pucekpoczta@gmail.com>
2021-03-28 13:36:29 +0200

    add variables

---
+++ modules/storage/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "The project ID to host the network in"
+  default     = "#FILL IN YOUR PROJECT ID HERE"
+}
+
+variable "region" {
+  description = "The project region"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "The project zone"
+  default     = "us-central1-a"
+}
