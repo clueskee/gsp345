59b9ab5ca68adc8e251f80f69dbee8540a303a9d
Clueskee <pucekpoczta@gmail.com>
2021-03-28 14:42:26 +0200

    add template to instances.tf

---
+++ modules/instances/instances.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  version = "3.5.0"
+
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance" "default" {
+  name         = "tf-instance-1"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+resource "google_compute_instance" "default" {
+  name         = "tf-instance-2"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
